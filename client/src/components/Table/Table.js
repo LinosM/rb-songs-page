41086bdbeddee40efc14d80bf221c4d595a95f4a
LinosM <Zemnas768@gmail.com>
2021-06-15 18:47:46 -0400

    Hide certain table columns based on window width size

--- client/src/components/Table/Table.js
+++ client/src/components/Table/Table.js
@@ -1,53 +1,73 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import TableBody from "../../components/TableBody";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSortAlphaUp, faSortAlphaDown, faSortNumericUpAlt, faSortNumericDownAlt } from '@fortawesome/free-solid-svg-icons';
-import "./index.css"
+import "./index.css";
 
-const Table = props => (
-  <table className="table is-narrow is-hoverable">
-    <thead>
-      <tr>
-        <th className="date clickable" scope="col" value="updated" onClick={props.sortTable}>
-          Updated
-          <span className="ml-1">
-            <FontAwesomeIcon value="updated" onClick={props.sortTable} icon={props.upDown.updated ? faSortNumericUpAlt : faSortNumericDownAlt} />
-          </span>
-        </th>
-        <th scope="col" className="clickable" value="artist" onClick={props.sortTable}>
-          Artist
-          <span className="ml-1">
-            <FontAwesomeIcon value="artist" onClick={props.sortTable} icon={props.upDown.artist ? faSortAlphaUp : faSortAlphaDown} />
-          </span>
-        </th>
-        <th scope="col" className="clickable" value="song" onClick={props.sortTable}>
-          Song
-          <span className="ml-1">
-            <FontAwesomeIcon value="song" onClick={props.sortTable} icon={props.upDown.song ? faSortAlphaUp : faSortAlphaDown} />
-          </span>
-        </th>
-        <th scope="col" className="clickable" value="source" onClick={props.sortTable}>
-          Source
-          <span className="ml-1">
-            <FontAwesomeIcon value="source" onClick={props.sortTable} icon={props.upDown.source ? faSortAlphaUp : faSortAlphaDown} />
-          </span>
-        </th>
-        <th className="date" scope="col">Release Date</th>
-        <th scope="col">Multitrack</th>
-        <th scope="col">G</th>
-        <th scope="col">B</th>
-        <th scope="col">D</th>
-        <th scope="col">K</th>
-        <th scope="col">V</th>
-        <th scope="col">Preview</th>
-      </tr>
-    </thead>
-    <tbody>
-      {props.songs && props.songs.map(song => (
-        <TableBody song={song} key={song._id} openModal={props.openModal}/>
-      ))}
-    </tbody>
-  </table>
-);
+function Table(props) {
+  const [isDesktop, setDesktop] = useState(window.innerWidth > 992);
+  const [isMobile, setMobile] = useState(window.innerWidth < 768);
+  const updateMedia = () => { 
+    setDesktop(window.innerWidth > 992);
+    setMobile(window.innerWidth < 768);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", updateMedia);
+    return () => window.removeEventListener("resize", updateMedia);
+  });
+
+  return (
+    <table className="table is-narrow is-hoverable">
+      <thead>
+        <tr>
+          <th className="date clickable col_1" scope="col" value="updated" onClick={props.sortTable}>
+            Updated
+            <span className="ml-1">
+              <FontAwesomeIcon value="updated" onClick={props.sortTable} icon={props.upDown.updated ? faSortNumericUpAlt : faSortNumericDownAlt} />
+            </span>
+          </th>
+          <th scope="col" className="clickable col_2" value="artist" onClick={props.sortTable}>
+            Artist
+            <span className="ml-1">
+              <FontAwesomeIcon value="artist" onClick={props.sortTable} icon={props.upDown.artist ? faSortAlphaUp : faSortAlphaDown} />
+            </span>
+          </th>
+          <th scope="col col_3" className="clickable" value="song" onClick={props.sortTable}>
+            Song
+            <span className="ml-1">
+              <FontAwesomeIcon value="song" onClick={props.sortTable} icon={props.upDown.song ? faSortAlphaUp : faSortAlphaDown} />
+            </span>
+          </th>
+          <th scope="col col_4" className="clickable" value="source" onClick={props.sortTable}>
+            Source
+            <span className="ml-1">
+              <FontAwesomeIcon value="source" onClick={props.sortTable} icon={props.upDown.source ? faSortAlphaUp : faSortAlphaDown} />
+            </span>
+          </th>
+          {isDesktop &&
+            <th className="date col_5" scope="col">Release Date</th>
+          }
+          {!isMobile &&
+            <>
+              <th scope="col" className="col_6">Multitrack</th>
+              <th scope="col">G</th>
+              <th scope="col">B</th>
+              <th scope="col">D</th>
+              <th scope="col">K</th>
+              <th scope="col">V</th>
+            </>
+          }
+          <th scope="col">Preview</th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.songs && props.songs.map(song => (
+          <TableBody song={song} key={song._id} openModal={props.openModal} />
+        ))}
+      </tbody>
+    </table>
+  )
+}
 
 export default Table;
--- client/src/components/TableBody/TableBody.js
+++ client/src/components/TableBody/TableBody.js
@@ -1,30 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import moment from "moment";
 import "./index.css";
 
-function hello() {
-  console.log("hello");
-}
+function TableBody(props) {
+  const [isDesktop, setDesktop] = useState(window.innerWidth > 992);
+  const [isMobile, setMobile] = useState(window.innerWidth < 768);
+  const updateMedia = () => {
+    setDesktop(window.innerWidth > 992);
+    setMobile(window.innerWidth < 768);
+  };
 
-const TableBody = props => (
-  <tr>
-    <td className="date">{moment(props.song.updated_date).format("YYYY-MM-DD")}</td>
-    <td>{props.song.artist}</td>
-    <td><a href={props.song.download} target="_blank">{props.song.song_name}</a></td>
-    <td>{props.song.source ? props.song.source : ""}</td>
-    <td className="date">{moment(props.song.release_date).format("YYYY-MM-DD")}</td>
-    <td>{props.song.multitrack}</td>
-    <td>{props.song.g}</td>
-    <td>{props.song.b}</td>
-    <td>{props.song.d}</td>
-    <td>{props.song.k}</td>
-    <td>{props.song.v}</td>
-    {/* <td><a href="#" value={props.song.video} className="preview" onClick={props.openModal} name={props.song.song_name} artist={props.song.artist}>Preview</a></td> */}
-    {/* <td><FontAwesomeIcon icon={faYoutube} value={props.song.video} className="preview" onClick={props.openModal} name={props.song.song_name} artist={props.song.artist} /></td> */}
-    <td value={props.song.video} onClick={props.openModal} name={props.song.song_name} artist={props.song.artist}>
-      <i className="fa fa-youtube-play fa-2x" value={props.song.video} onClick={props.openModal} name={props.song.song_name} artist={props.song.artist}></i>
-    </td>
-  </tr>
-);
+  useEffect(() => {
+    window.addEventListener("resize", updateMedia);
+    return () => window.removeEventListener("resize", updateMedia);
+  });
+
+  return (
+    <tr>
+      <td className="date">{moment(props.song.updated_date).format("YYYY-MM-DD")}</td>
+      <td>{props.song.artist}</td>
+      <td><a href={props.song.download} target="_blank">{props.song.song_name}</a></td>
+      <td>{props.song.source ? props.song.source : ""}</td>
+      {isDesktop &&
+        <td className="date">{moment(props.song.release_date).format("YYYY-MM-DD")}</td>
+      }
+      {!isMobile &&
+        <>
+          <td>{props.song.multitrack}</td>
+          <td>{props.song.g}</td>
+          <td>{props.song.b}</td>
+          <td>{props.song.d}</td>
+          <td>{props.song.k}</td>
+          <td>{props.song.v}</td>
+        </>
+      }
+      <td value={props.song.video} onClick={props.openModal} name={props.song.song_name} artist={props.song.artist}>
+        <i className="fa fa-youtube-play fa-2x" value={props.song.video} onClick={props.openModal} name={props.song.song_name} artist={props.song.artist}></i>
+      </td>
+    </tr>
+  )
+}
 
 export default TableBody;
