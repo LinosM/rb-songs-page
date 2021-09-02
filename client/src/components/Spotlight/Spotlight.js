61c7b26a0cb8826593e0c5e72d14f8dfc34315bd
Duy Nguyen <Zemnas768@gmail.com>
2021-09-01 20:50:46 -0400

    added date to spotlight release

--- client/src/components/Spotlight/Spotlight.js
+++ client/src/components/Spotlight/Spotlight.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./index.css";
+import moment from "moment";
 
 function Spotlight(props) {
 
@@ -19,6 +20,7 @@ function Spotlight(props) {
   return (
     <>
       <div className="has-text-centered title is-4 has-text-light is-underlined">Latest Release</div>
+      <p className="has-text-centered is-italic title is-4 has-text-light">{moment(props.song.release_date).format('MMMM Do, YYYY')}</p>
       <div className="columns">
         <div className="column is-6">
           <a href={props.song.download} target="_blank" rel="noopener noreferrer">
