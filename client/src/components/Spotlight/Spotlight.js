6109f7d15ce6acbdad076515bf9413550f1539d4
Duy Nguyen <Zemnas768@gmail.com>
2021-09-28 04:25:33 -0400

    Spotlight img and video columns will now stack when at or below 1006px width

--- client/src/components/Spotlight/Spotlight.js
+++ client/src/components/Spotlight/Spotlight.js
@@ -4,6 +4,7 @@ import moment from "moment";
 
 function Spotlight(props) {
 
+  //Regex function get retrieves video ID from most youtube links
   function YouTubeGetID(url) {
     var ID = '';
     url = url.replace(/(>|<)/gi, '').split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/);
@@ -21,19 +22,19 @@ function Spotlight(props) {
     <>
       <div className="has-text-centered title is-4 has-text-light is-underlined">Latest Release</div>
       <p className="has-text-centered is-italic title is-4 has-text-light">{moment(props.song.release_date).format('MMMM Do, YYYY')}</p>
-      <div className="columns">
-        <div className="column is-6">
+      <div className="columns is-desktop">
+        <div className="column">
           <a href={props.song.download} target="_blank" rel="noopener noreferrer">
             <figure className="image">
               <img src={props.song.preview} className="has-image-centered previewImage"></img>
             </figure>
           </a>
         </div>
-        <div className="column is-6">
+        <div className="column">
           <div className="video-responsive">
             <iframe
               width="853"
-              height="400"
+              height="auto"
               src={`https://www.youtube.com/embed/${YouTubeGetID(props.song.video)}`}
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
