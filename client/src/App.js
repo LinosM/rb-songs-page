82e49656482e15104235467b6870d22047a23dbc
Duy Nguyen <Zemnas768@gmail.com>
2021-10-06 01:03:33 -0400

    Added Nav and Wrapper components

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import React, { useEffect } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Nav from "./components/Nav";
+import Wrapper from "./components/Wrapper";
 import Main from "./pages/Main";
+import About from "./pages/About";
 
 function App() {
   useEffect(() => {
@@ -10,13 +12,16 @@ function App() {
 
   return (
     <div className="App">
-        <div>
-          <Nav/>
+      <div>
+        <Nav />
+        <Wrapper>
           <div className="main-view">
             <Switch>
               <Route exact path="/" component={Main} />
+              <Route exact path="/about" component={About} />
             </Switch>
           </div>
+        </Wrapper>
       </div>
     </div>
   );
